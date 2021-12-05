170b262cd29cdc5d119be08cebd3cd917d9294ed
Вадим Байбуз <vadimbajbuz@192.168.1.7>
2021-12-05 15:48:28 +0300

    Сделал маленький рефакторинг

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import FiltersTemplate from './view/filter-view.js';
 import SortTemplate from './view/sort.js';
 import NewPointTemplate from './view/new-point.js';
 import PointTemplate from './view/point-view.js';
-import PointsListItemView from './view/points-list-item.js';
+// import PointsListItemView from './view/points-list-item.js';
 import PointsListView from './view/points-list.js';
 import {generatePoint} from './mock/task.js';
 import {generateFilter} from './mock/filter.js';
 // import {DEFAULT_VALUE} from './const.js';
 
-import {renderTemplate, renderElement, RenderPosition} from './render.js';
+import {render, RenderPosition} from './render.js';
 import {SORTING} from './const';
 
 const POINTS_COUNT = 3;
@@ -20,19 +20,19 @@ const headerElement = document.querySelector('.trip-main__trip-controls');
 const navHeaderElement = headerElement.querySelector('.trip-controls__navigation');
 const filtersHeaderElement = headerElement.querySelector('.trip-controls__filters');
 
-renderElement(navHeaderElement, new SiteMenuView().element, RenderPosition.BEFOREEND);
-renderElement(filtersHeaderElement, new FiltersTemplate(filters).element, RenderPosition.BEFOREEND);
+render(navHeaderElement, new SiteMenuView().element, RenderPosition.BEFOREEND);
+render(filtersHeaderElement, new FiltersTemplate(filters).element, RenderPosition.BEFOREEND);
 
 const mainElement = document.querySelector('.page-main');
 const eventsElementContainer = document.querySelector('.trip-events');
 
-renderElement(eventsElementContainer, new SortTemplate(SORTING[0]).element, RenderPosition.BEFOREEND);
-renderElement(eventsElementContainer, new PointsListView().element, RenderPosition.BEFOREEND);
+render(eventsElementContainer, new SortTemplate(SORTING[0]).element, RenderPosition.BEFOREEND);
+render(eventsElementContainer, new PointsListView().element, RenderPosition.BEFOREEND);
 
 const eventsElementList = mainElement.querySelector('.trip-events__list');
 
-renderElement(eventsElementList, new NewPointTemplate(points[0]).element, RenderPosition.BEFOREEND);
+render(eventsElementList, new NewPointTemplate(points[0]).element, RenderPosition.BEFOREEND);
 
 for (let i = 0; i < POINTS_COUNT; i++) {
-  renderElement(eventsElementList, new PointTemplate(points[i]).element, RenderPosition.BEFOREEND);
+  render(eventsElementList, new PointTemplate(points[i]).element, RenderPosition.BEFOREEND);
 }
--- src/view/points-list-item.js
+++ src/view/points-list-item.js
@@ -1,33 +1,33 @@
-import {createElement} from '../render';
-
-
-/**
- * @param {String} itemContent
- * @returns {String}
- */
-const createPointsListItemTemplate = (itemContent) => (
-  `<li class="trip-events__item">
-    ${itemContent}
-  </li>`
-);
-
-export default class PointsListItemView {
-  #element = null;
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  get template() {
-    return createPointsListItemTemplate();
-  }
-
-  removeElement() {
-    this.#element = null;
-  }
-}
-
+// import {createElement} from '../render';
+//
+//
+// /**
+//  * @param {String} itemContent
+//  * @returns {String}
+//  */
+// const createPointsListItemTemplate = (itemContent) => (
+//   `<li class="trip-events__item">
+//     ${itemContent}
+//   </li>`
+// );
+//
+// export default class PointsListItemView {
+//   #element = null;
+//
+//   get element() {
+//     if (!this.#element) {
+//       this.#element = createElement(this.template);
+//     }
+//
+//     return this.#element;
+//   }
+//
+//   get template() {
+//     return createPointsListItemTemplate();
+//   }
+//
+//   removeElement() {
+//     this.#element = null;
+//   }
+// }
+//
